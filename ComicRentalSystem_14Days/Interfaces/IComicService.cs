be82f9683e1977118f7b2b17fad8c8c5dcd1c264
HungWeiChen310 <willchen310@gmail.com>
2025-06-04 10:08:33 +0800

    Add IComicService interface and implement in ComicService

---
+++ ComicRentalSystem_14Days/Interfaces/IComicService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ComicRentalSystem_14Days.Models;
+
+namespace ComicRentalSystem_14Days.Interfaces
+{
+    public delegate void ComicDataChangedEventHandler(object? sender, EventArgs e);
+
+    public interface IComicService
+    {
+        event ComicDataChangedEventHandler? ComicsChanged;
+
+        Task ReloadAsync();
+
+        List<Comic> GetAllComics();
+        Comic? GetComicById(int id);
+        void AddComic(Comic comic);
+        void UpdateComic(Comic comic);
+        void DeleteComic(int id);
+        List<Comic> GetComicsByGenre(string genreFilter);
+        List<Comic> SearchComics(string? searchTerm = null);
+        List<AdminComicStatusViewModel> GetAdminComicStatusViewModels(IEnumerable<Member> allMembers);
+    }
+}
